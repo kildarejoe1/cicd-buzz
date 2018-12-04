6aea22f9ccc37a68a7f487f47ee7c71436ce9f24
henry morrin <henry.morrin@iii.com>
2018-12-04 13:08:37 +0000

    Update docker bash file

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS  https://index.docker.io
 
 if [ "$TRAVIS_BRANCH" = "master" ]; then
 
