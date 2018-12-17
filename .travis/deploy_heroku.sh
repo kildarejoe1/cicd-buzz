698b484a596d6ea217a8393e7bb9421ae02681a3
iiihenrymorrin <henry.morrin@iii.com>
2018-12-17 16:46:55 +0000

    Updated deploy_heroku script to release docker containery, added additional heroku release command

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -8,4 +8,4 @@ docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 
 heroku container:push web --app $HEROKU_APP_NAME
 
-heroku container:release web
+heroku container:release web --app=$HEROKU_APP_NAME
