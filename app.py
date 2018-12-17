d8e935aefad38da09a005a179b77e21c0a24b8c5
iiihenrymorrin <henry.morrin@iii.com>
2018-12-17 18:23:55 +0000

    Added bootstrap to flask template

--- app.py
+++ app.py
@@ -21,10 +21,9 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 def generate_buzz():
 
     page = '<html><body><h1>'
-
+    page +='<head> <title>Bootstrap Example</title> <meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script></head>'
     page += generator.generate_buzz()
-
-    page += '</h1></body><p>Heroku App</html>'
+    page += '</h1></body></html>'
 
     return page
 
