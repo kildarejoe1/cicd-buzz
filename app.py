5afb4b2dfa196a86499afbdb1d34268676858893
henry morrin <henry.morrin@iii.com>
2020-03-12 16:29:45 +0000

    Added code

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 def generate_buzz():
 
     page = '<html><body>'
-    page +='<head> <title>Bootstrap Example</title> <meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script></head>'
+    page +='<head> <title>Henrys Example</title> <meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"><script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script></head>'
     page += '<div class="container"><h1>'
     page += generator.generate_buzz()
     page += '</h1></div></body></html>'
